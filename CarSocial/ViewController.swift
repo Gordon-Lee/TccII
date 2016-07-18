24871beb815b91e3525365418c563f3932b964b8
marcoaureliodiasamerico@gmail.com <marcoaureliodiasamerico@gmail.com>
2016-07-18 19:36:28 -0300

    First Commit

--- CarSocial/ViewController.swift
+++ CarSocial/ViewController.swift
@@ -12,14 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("First Commit")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
